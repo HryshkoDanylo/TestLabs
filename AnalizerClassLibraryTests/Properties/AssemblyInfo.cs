549c733a0d57cb6bcf04bc4f645afd72db8b164e
HryshkoDanylo <danylo.hryshko.pm.2022@lpnu.ua>
2024-12-01 22:07:06 +0200

    first commit

---
+++ AnalizerClassLibraryTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AnalizerClassLibraryTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AnalizerClassLibraryTests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("81965885-169c-4219-84f8-e4b1bc0d50f4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AnalizerClassLibraryTests/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AnalaizerClassLibrary;
+using System.Collections.Generic;
+using System;
+
+namespace AnalizerClassLibraryTests
+{
+    [TestClass]
+    public class AnalizerClassTests
+    {
+        public TestContext TestContext { get; set; }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "C:\\Users\\User\\Desktop\\tests\\Calculator\\AnalizerClassLibraryTests\\test-cases.xml", "TestInsertSymbol", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void InsertSymbol_TestCases_ReturnsExpectedResults()
+        {
+            string input = Convert.ToString(TestContext.DataRow["Input"]);
+            char symbol = Convert.ToChar(TestContext.DataRow["Symbol"]);
+            int position = Convert.ToInt32(TestContext.DataRow["Position"]);
+            string expected = Convert.ToString(TestContext.DataRow["Expected"]);
+
+            string result = AnalaizerClass.InsertSymbol(input, symbol, position);
+            
+            Assert.AreEqual(expected, result);
+        }
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "C:\\Users\\User\\Desktop\\tests\\Calculator\\AnalizerClassLibraryTests\\test-cases.xml", "TestIsOperator", DataAccessMethod.Sequential)]
+        [TestMethod]
+
+        public void IsOperator_TestCases_ReturnsExpectedResults()
+        {
+            string input = Convert.ToString(TestContext.DataRow["Input"]);
+            bool expected = Convert.ToBoolean(TestContext.DataRow["Expected"]);
+
+            bool result = AnalaizerClass.IsOperator(input);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
